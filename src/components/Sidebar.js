ec09219b519d819e33cfb7c809e4343e5e2f381b
Abrar <myselfabrar.23@gmail.com>
2025-11-10 18:52:51 +0530

    Fixed unused imports for Vercel build

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { FaTachometerAlt, FaFolderOpen, FaClipboardList, FaUser, FaBell, FaSignOutAlt } from "react-icons/fa";
-import { useNavigate, useLocation } from "react-router-dom";
+import { FaTachometerAlt, FaFolderOpen, FaClipboardList, FaUser, FaBell } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
 import "../styles/Dashboard.css";
 
 export default function Sidebar() {
-  const navigate = useNavigate();
+  
   const location = useLocation();
 
-  const handleSignOut = () => {
-    const confirmLogout = window.confirm("Are you sure you want to sign out?");
-    if (confirmLogout) navigate("/signup");
-  };
+  
 
   const menuItems = [
     { name: "Dashboard", icon: <FaTachometerAlt />, path: "/dashboard" },
